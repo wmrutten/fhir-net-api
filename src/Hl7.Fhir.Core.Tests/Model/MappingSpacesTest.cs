d879a569effd30ea5372a9844aed9eabc7085a33
Michel Rutten <m.rutten@furore.com>
2016-06-16 16:52:48 +0200

    feature: implement class for global mapping definitions, load from mappingspaces.xml

--- src/Hl7.Fhir.Core.Tests/Model/MappingSpacesTest.cs
+++ src/Hl7.Fhir.Core.Tests/Model/MappingSpacesTest.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Text;
 using System.Xml;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Linq;
 
 namespace Hl7.Fhir.Model
 {
@@ -13,107 +15,152 @@ namespace Hl7.Fhir.Model
     {
         const string mappingSpacesXml = @"TestData\mappingSpaces.xml";
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times")]
-        [TestMethod]
-        public void TestSerializeMappingSpace()
+        static readonly XmlWriterSettings exampleXmlWriterSettings = new XmlWriterSettings()
         {
-            var mappingSpace = new MappingSpace()
-            {
-                Url = new Uri(@"http://hl7.org/v3"),
-                ColumnName = "RIM Mapping",
-                Title = "RIM",
-                Id = "rim",
-                sort = 1
-            };
+            Encoding = Encoding.UTF8,
+            OmitXmlDeclaration = true,
+            Indent = false
+        };
 
-            const string expected = @"<space><url>http://hl7.org/v3</url><columnName>RIM Mapping</columnName><title>RIM</title><id>rim</id><sort>1</sort></space>";
+        static readonly MappingSpace exampleMappingSpace = new MappingSpace()
+        {
+            Url = new Uri(@"http://hl7.org/v3"),
+            ColumnName = "RIM Mapping",
+            Title = "RIM",
+            Id = "rim",
+            Sort = 1
+        };
 
-            XmlWriterSettings xws = new XmlWriterSettings()
-            {
-                Encoding = Encoding.UTF8,
-                OmitXmlDeclaration = true,
-                Indent = false
-            };
+        const string exampleMappingSpaceXml = @"<space><url>http://hl7.org/v3</url><columnName>RIM Mapping</columnName><title>RIM</title><id>rim</id><sort>1</sort></space>";
 
-            string actual;
+        static readonly MappingSpaceList exampleMappingSpaces = new MappingSpaceList() { exampleMappingSpace };
+
+        const string exampleMappingSpacesXml = @"<mappingSpaces>" + exampleMappingSpaceXml + "</mappingSpaces>";
+
+        const string xmlSchemaInstanceNamespace = " xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\"";
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times")]
+        string XmlSerialize<T>(T data)
+        {
             using (var sw = new StringWriter())
-            using (var writer = XmlWriter.Create(sw, xws))
+            using (var writer = XmlWriter.Create(sw, exampleXmlWriterSettings))
             {
-                // ser.Serialize(writer, mappingSpace);
-
                 // Suppress default xsd/xsi namespace declarations
                 XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
                 ns.Add(string.Empty, string.Empty);
-
-                var ser = new XmlSerializer(typeof(MappingSpace));
-                ser.Serialize(writer, mappingSpace, ns);
-
-                actual = sw.ToString();
+                var ser = new XmlSerializer(typeof(T));
+                ser.Serialize(writer, data, ns);
+                return sw.ToString();
             }
-
-            Console.WriteLine(actual);
-
-            Assert.AreEqual(expected, actual);
         }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times")]
-        [TestMethod]
-        public void TestSerializeMappingSpaces()
+        string DataContractSerialize<T>(T data)
         {
-            var mappingSpaces = new MappingSpaces()
+            using (var sw = new StringWriter())
+            using (var writer = XmlWriter.Create(sw, exampleXmlWriterSettings))
             {
-                Spaces = {
-                    new MappingSpace()
-                    {
-                        Url = new Uri(@"http://hl7.org/v3"),
-                        ColumnName = "RIM Mapping",
-                        Title = "RIM",
-                        Id = "rim",
-                        sort = 1
-                    }
+                var settings = new DataContractSerializerSettings()
+                {
+                    SerializeReadOnlyTypes = true
+                };
+                var ser = new DataContractSerializer(typeof(T), settings);
+                ser.WriteObject(writer, data);
+
+                writer.Flush();
+                var result = sw.ToString();
+
+                // DataContractSerializer always emits XMLSchema namespace, cannot suppress...
+                // xmlns:i="http://www.w3.org/2001/XMLSchema-instance"
+                return result.Replace(xmlSchemaInstanceNamespace, string.Empty);
+            }
+        }
+
+        [TestMethod]
+        public void TestMappingSpaceXmlSerializer()
+        {
+            string actual = XmlSerialize(exampleMappingSpace);
+            Console.WriteLine(actual);
+            Assert.AreEqual(exampleMappingSpaceXml, actual);
+        }
+
+        [TestMethod]
+        public void TestMappingSpaceDataContractSerializer()
+        {
+            string actual = DataContractSerialize(exampleMappingSpace);
+            Console.WriteLine(actual);
+            Assert.AreEqual(exampleMappingSpaceXml, actual);
+        }
+
+        [TestMethod]
+        public void TestMappingSpacesXmlSerializer()
+        {
+            string actual = XmlSerialize(exampleMappingSpaces);
+            Console.WriteLine(actual);
+            Assert.AreEqual(exampleMappingSpacesXml, actual);
+        }
+
+        [TestMethod]
+        public void TestMappingSpacesDataContractSerializer()
+        {
+            string actual = DataContractSerialize(exampleMappingSpaces);
+            Console.WriteLine(actual);
+            Assert.AreEqual(exampleMappingSpacesXml, actual);
+        }
+
+        // private static void AssertMappingSpaces(IReadOnlyList<MappingSpace> mappingSpaces)
+        private static void AssertMappingSpaces(ICollection<MappingSpace> mappingSpaces)
+        {
+            Assert.IsNotNull(mappingSpaces);
+            Assert.AreEqual(mappingSpaces.Count, 30);
+            foreach (var space in mappingSpaces.OrderBy(space => space.Sort))
+            {
+                Console.WriteLine($"{space.Sort,3} | {space.Id} | {space.Title} | {space.ColumnName} | {space.Url}");
+            }
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times")]
+        void TestMappingSpacesDeserializer(Func<XmlReader, MappingSpaceList> deserialize)
+        {
+            MappingSpaceList mappingSpaces;
+            using (var sr = new StreamReader(mappingSpacesXml))
+            using (var reader = XmlReader.Create(sr))
+            {
+                mappingSpaces = deserialize(reader);
+            }
+            AssertMappingSpaces(mappingSpaces);
+        }
+
+        [TestMethod]
+        public void TestMappingSpacesXmlDeserializer()
+        {
+            TestMappingSpacesDeserializer(
+                reader => 
+                {
+                    var ser = new XmlSerializer(typeof(MappingSpaceList));
+                    return (MappingSpaceList)ser.Deserialize(reader);
                 }
-            };
-
-            //var mappingSpaces = new MappingSpaces()
-            //{
-            //    new MappingSpace()
-            //    {
-            //        Url = new Uri(@"http://hl7.org/v3"),
-            //        ColumnName = "RIM Mapping",
-            //        Title = "RIM",
-            //        Id = "rim",
-            //        sort = 1
-            //    }
-            //};
-
-            const string expected = @"<mappingSpaces><space><url>http://hl7.org/v3</url><columnName>RIM Mapping</columnName><title>RIM</title><id>rim</id><sort>1</sort></space></mappingSpaces>";
-
-            XmlWriterSettings xws = new XmlWriterSettings()
-            {
-                Encoding = Encoding.UTF8,
-                OmitXmlDeclaration = true,
-                Indent = false
-            };
-
-            string actual;
-            using (var sw = new StringWriter())
-            using (var writer = XmlWriter.Create(sw, xws))
-            {
-                // ser.Serialize(writer, mappingSpace);
-
-                // Suppress default xsd/xsi namespace declarations
-                XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
-                ns.Add(string.Empty, string.Empty);
-
-                var ser = new XmlSerializer(typeof(MappingSpaces));
-                ser.Serialize(writer, mappingSpaces, ns);
-
-                actual = sw.ToString();
-            }
-
-            Console.WriteLine(actual);
-
-            Assert.AreEqual(expected, actual);
+            );
         }
+
+        [TestMethod]
+        public void TestMappingSpacesDataContractDeserializer()
+        {
+            TestMappingSpacesDeserializer(
+                reader =>
+                {
+                    var ser = new DataContractSerializer(typeof(MappingSpaceList));
+                    return (MappingSpaceList)ser.ReadObject(reader);
+                }
+            );
+        }
+
+        [TestMethod]
+        public void TestLoadMappingSpaces()
+        {
+            var mappingSpaces = MappingSpaces.LoadXml(mappingSpacesXml);
+            AssertMappingSpaces(mappingSpaces);
+        }
+
     }
 }
--- src/Hl7.Fhir.Core/Model/MappingSpace.cs
+++ src/Hl7.Fhir.Core/Model/MappingSpace.cs
@@ -1,55 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Runtime.Serialization;
 using System.Xml;
 using System.Xml.Serialization;
 
 namespace Hl7.Fhir.Model
 {
-    [DataContract, XmlRoot("mappingSpaces")]
-    public class MappingSpaces
+    public static class MappingSpaces
     {
-        [XmlElement("space")]
-        public List<MappingSpace> Spaces { get; set; } = new List<MappingSpace>();
+        /// <summary>Initialize a list of mapping space definitions from the specified Xml file (mappingspaces.xml).</summary>
+        /// <param name="filePath">The absolute filepath to the mapping definitions Xml file (mappingspaces.xml).</param>
+        /// <returns>A list of <see cref="MappingSpace"/> definitions.</returns>
+        // public static IReadOnlyList<MappingSpace> LoadXml(string filePath) // IReadOnlyList is not available in Net40 & Portable45 
+        public static MappingSpaceList LoadXml(string filePath) // IReadOnlyList
+        {
+            MappingSpaceList result;
+            using (var sr = new StreamReader(filePath))
+            using (var reader = XmlReader.Create(sr))
+            {
+#if true
+                // Use DataContractSerializer
+                var ser = new DataContractSerializer(typeof(MappingSpaceList));
+                result = (MappingSpaceList)ser.ReadObject(reader);
+#else
+                // Use XmlSerializer
+                // var ser = new XmlSerializer(typeof(MappingSpaces));
+                result = (MappingSpaces)ser.Deserialize(reader);
+#endif
+            }
+            return result; // result.AsReadOnly();
+        }
     }
 
-    //[DataContract, XmlRoot("mappingSpaces")]
-    //public class MappingSpaces
-    //{
-    //    [DataMember, XmlElement("space")]
-    //    public MappingSpaceList Spaces { get; set; } = new MappingSpaceList();
-    //}
-
-    //[CollectionDataContract(ItemName = "space")]
-    //public class MappingSpaceList : List<MappingSpace> { }
-
-
-    [DataContract, XmlRoot("space", IsNullable = false)]
-    public class MappingSpace
+    /// <summary>A list of <see cref="MappingSpace"/> definitions.</summary>
+    [CollectionDataContract(Namespace = "", Name = "mappingSpaces", ItemName = "space")]
+    [XmlRoot("mappingSpaces")]
+    public sealed class MappingSpaceList : List<MappingSpace>
     {
-        [IgnoreDataMember, XmlIgnore]
+    }
+
+    /// <summary>A mapping space definition.</summary>
+    [DataContract(Namespace = "", Name = "space")]
+    [XmlRoot("space", IsNullable = false)]
+    [XmlType("space")]
+    public sealed class MappingSpace
+    {
+        // Serializable by DataContractSerializer, but not supported by XmlReader
+        [DataMember(Name = "url", Order = 1)]
+        [XmlIgnore]
         public Uri Url { get; set; }
 
-        // Serializable hidden shadow property
-        [DataMember, XmlElement("url")]
-        [Browsable(false), EditorBrowsable(EditorBrowsableState.Never)]
+        // Serializable hidden shadow property for XmlSerializer, not necessary for DataContractSerializer
+        [IgnoreDataMember]
+        [XmlElement("url")]
+        // [Browsable(false)] // Not available in Portable45
+        [EditorBrowsable(EditorBrowsableState.Never)]
         public string UriString
         {
             get { return Url == null ? null : Url.OriginalString; }
             set { Url = value == null ? null : new Uri(value, UriKind.RelativeOrAbsolute); }
         }
 
-        [DataMember, XmlElement("columnName")]
+        [DataMember(Name = "columnName", Order = 2)]
+        [XmlElement("columnName")]
         public string ColumnName { get; set; }
 
-        [DataMember, XmlElement("title")]
+        [DataMember(Name = "title", Order = 3)]
+        [XmlElement("title")]
         public string Title { get; set; }
 
-        [DataMember, XmlElement("id")]
+        [DataMember(Name = "id", Order = 4)]
+        [XmlElement("id")]
         public string Id { get; set; }
 
-        [DataMember, XmlElement("sort")]
-        public int sort { get; set; }
+        [DataMember(Name = "sort", Order = 5)]
+        [XmlElement("sort")]
+        public int Sort { get; set; }
     }
 }
